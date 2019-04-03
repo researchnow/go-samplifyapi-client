5e8f697a7eb91b4139be72c3fdcaf71d7cdf7847
Yugma Moradia <yugma93@gmail.com>
2019-04-03 15:27:12 -0700

    Add URL package which has a format functioon to return formatted livelink and Testlink

--- lib/url/format.go
+++ lib/url/format.go
@@ -4,8 +4,20 @@ import (
 	"net/url"
 )
 
+const (
+	//TemplatePSID Default Paramter value of PSID
+	TemplatePSID = "<#IdParameter[Value]>"
+	//TemplatePID Default Paramter value of PID
+	TemplatePID = "<#DubKnowledge[1500/Entity id]>"
+	//TemplateSecurityKey Default Paramter value of K2
+	TemplateSecurityKey = "<#Project[Secure Key 2]>"
+)
+
+// CustomParams is a type of map that will hold the parameter value as key and parameter template as value.
+type CustomParams map[string]string
+
 // Format returns the formatted TestLink appending the custom paramaters
-func Format(u *url.URL, m map[string]string) (*string, error) {
+func Format(u *url.URL, m CustomParams) (*string, error) {
 	q := u.Query()
 
 	for key, value := range m {
