96df7a0fb0a10f15f4cceecaa7e6f2bfd98e305a
Munkhdelger Buyandalai <37114996+deegii22@users.noreply.github.com>
2020-07-24 12:13:15 -0700

    added api change for user info endpoint

--- lib/user.go
+++ lib/user.go
@@ -8,18 +8,27 @@ type UserResponse struct {
 
 // User to hold any information related to the user.
 type User struct {
-	Applications []Application `json:"applications"`
-	CompanyID    int32         `json:"companyId"`
-	CompanyName  string        `json:"companyName"`
-	Email        string        `json:"email"`
-	Username     string        `json:"username"`
-	FullName     string        `json:"fullName"`
+	Email     string    `json:"email"`
+	Username  string    `json:"username"`
+	FullName  string    `json:"fullName"`
+	ID        int32     `json:"id"`
+	Companies []Company `json:"companies"`
 }
 
-// Application to hold the app level information of the user.
-type Application struct {
-	ID      int32  `json:"appId"`
-	Current bool   `json:"current"`
+// Company to hold the company information of the user.
+type Company struct {
+	ID          int32  `json:"id"`
+	Default     bool   `json:"default"`
+	DefaultRole string `json:"defaultRole"`
+	Name        string `json:"name"`
+	Teams       []Team `json:"teams"`
+}
+
+// Team to hold the team information of the company.
+type Team struct {
+	ID      int32  `json:"id"`
 	Default bool   `json:"default"`
+	Role    string `json:"role"`
 	Name    string `json:"name"`
+	Status  string `json:"status"`
 }
